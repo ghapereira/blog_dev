215a7a2f798f705f20795f61d2188fbd06703da2
Gustavo Pereira <ghapereira@gmail.com>
2017-07-18 16:59:00 -0300

    First commit

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+apt-get update
+apt-get upgrade
+
+apt-get install -y git zsh
+apt-get install -y wget
+
+# Oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
+cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
+chsh -s /usr/bin/zsh vagrant
+
