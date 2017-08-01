1b95bb12dfc329dc2a378263fed7ed84d1c39215
Gustavo Pereira <ghapereira>
2017-08-01 14:35:17 -0300

    Add tig to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ apt-get upgrade
 apt-get install -y git zsh
 apt-get install -y wget stow
 apt-get install -y python3.4-venv
-apt-get install -y silversearcher-ag
+apt-get install -y silversearcher-ag tig
 
 # Oh-my-zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
